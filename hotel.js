6d2c6a2c09fc5620a70722e429746e16eaa8cab9
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-02 23:53:22 +0000

    HotelPRAC

--- hotel.js
+++ hotel.js
@@ -1,3 +1,28 @@
-var Hotel = {
+var HotelObject = {
+    OccupiedRooms: '12',
+    AvailableRoomsi: '8',
+    AvailableRoomsi2: '8',
+    Amentities: 'pool table',
+    TotalRooms: '20',
+    HeyBookRooma: 'Thanks for booking a room, check price',
+    HaveBookaaaRoom: 'You have booked 1 room',
+    CostofRoom: 'thats going to be 40$',
     
-}
+AvailableRoom: function(){
+        return this.TotalRooms - this.OccupiedRooms;
+    },
+
+BookaRoom: function(){
+    return this.HeyBookRooma;
+},
+    
+WantaRoom: function(){
+     return this.HaveBookaaaRoom + " & " + this.CostofRoom;
+    }, 
+    
+ UnBookRoom: function(){
+    this.AvailableRoomsi;
+    this.AvailableRoomsi2--;
+    console.log(" This is this many left " + this.AvailableRoomsi2 + " You just unbooked this is how many there is now " + this.AvailableRoomsi)
+ }    
+};
